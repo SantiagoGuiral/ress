3034955a1c1e6e7557ddf5ae0bc20af86aa4c590
santiagoguiral <santiagowslg@gmail.com>
2022-03-29 15:58:46 -0500

    added function to get moments

--- diff.py
+++ diff.py
@@ -29,3 +29,23 @@ def difference(prev_frame, frame):
 	contours = get_contours(thresh)
 
 	contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	
+	list_contours = list(contours)
+	cnt1 = max(list_contours, key = lambda x: cv2.contourArea(x))
+	list_contours.remove(cnt1)
+
+	cnt2 = max(list_contours, key = lambda x: cv2.contourArea(x))
+
+	M1 = cv2.moments(cnt1)
+	x1 = int(M1["10"]/M1["m00"])
+	y1 = int(M1["01"]/M1["m00"])
+	c1 = (x1, y1)
+
+	M2 = CV2.moments(cnt2)
+	x2 = int(M2["10"]/M2["m00"])
+	y2 = int(M2["01"]/M2["m00"])
+	c2 = (x2, y2)
+
+	return c1, c2
+
+
