de2f5db73ee79cd5479a25d6ad90bc1b37980e23
Ego2509 <egoego2509@gmail.com>
2022-04-04 18:42:44 -0500

    PGN parser working.

--- moves2PGN_parser.py
+++ moves2PGN_parser.py
@@ -1,77 +1,84 @@
-import numpy
+def println(t): print(t+"\n"); return 0;
+def start_read(source_file):
+    with open(source_file,"r") as f:
+        record=f.read()
+        return record
+
+def move(m):
+    M="" #rendered_move
+    capture=0
+    #castle
+    if(m.lower()=="ooo" or m.lower()=="oo"):
+        M='-'.join(m.upper()) #https://stackoverflow.com/a/3258612/8235105
+        return M
+    else: #fix later, it should not be needed
+        if(m[0].lower()!="x"):
+            past=m[0:2]
+            present=m[-2:len(m)]
+            piece=m[2:-2]
+        else:
+            past=m[1:3]
+            present=m[-2:len(m)]
+            piece=m[3:-2]
+            capture=1
+        #rendering the move:
+        print(piece)
+        print(past)
+        print(present)
+        pt=piece[0] #piece type
+        if(pt.lower()=="p"):
+            M=f"{past[0]}x{present}" if(capture) else f"{present}"
+        else:
+            M=f"{pt.upper()}{past}x{present}" if(capture) else f"{pt.upper()}{past}{present}" #remove upper() if needed
+        return M
+
+
+def decode_stringhistory(record):
+    #orders moves
+    record_listed=record.split(" - ")
+    print(record_listed)
+    record_listed[-1]=record_listed[-1][:-1]
+    record_ordered=""
+    for i,m in enumerate(record_listed):
+        iteration=i//2+1
+        Blackstate=i%2
+        if(not Blackstate):
+            record_ordered+=f"{iteration}. "
+        println("move: "+m)
+        processed_move=move(m)
+        record_ordered+=f"{processed_move} " #move
+        println(record_ordered)
+    return record_ordered
+
+
+#moves source file
+source="chess-pgn.txt"
 #record
-record=""
-#initial position
-pieces={
-    'RL':'a1',
-    'NL':'b1',
-    'BL':'c1',
-    'Q' :'d1',
-    'K' :'e1',
-    'BR':'f1',
-    'NR':'g1',
-    'RR':'h1',
-    'P1':'a2',
-    'P2':'b2',
-    'P3':'c2',
-    'P4':'d2',
-    'P5':'e2',
-    'P6':'f2',
-    'P7':'g2',
-    'P8':'h2',
-    
-    'rl':'a8',
-    'nl':'b8',
-    'bl':'c8',
-    'q' :'d8',
-    'k' :'e8',
-    'br':'f8',
-    'nr':'g8',
-    'rr':'h8',
-    'p1':'a7',
-    'p2':'b7',
-    'p3':'c7',
-    'p4':'d7',
-    'p5':'e7',
-    'p6':'f7',
-    'p7':'g7',
-    'p8':'h7'
-}
-#
-pos={
-    'K' :['e1','e1'],
-    'k' :['e8','e8'],
-    'Q' :['d1','d1'],
-    'q' :['d8','d8'],
-    'RL':['a1','a1'],
-    'RR':['h1','h1'],
-    'rl':['a8','a8'],
-    'rr':['h8','h8'],
-    'NL':['b1','b1'],
-    'NR':['g1','g1'],
-    'nl':['b8','b8'],
-    'nr':['g8','g8'],
-    'BL':['c1','c1'],
-    'BR':['f1','f1'],
-    'bl':['c8','c8'],
-    'br':['f8','f8'],
-    'P1':['a2','a2'],
-    'P2':['b2','b2'],
-    'P3':['c2','c2'],
-    'P4':['d2','d2'],
-    'P5':['e2','e2'],
-    'P6':['f2','f2'],
-    'P7':['g2','g2'],
-    'P8':['h2','h2'],
-    'p1':['a7','a7'],
-    'p2':['b7','b7'],
-    'p3':['c7','c7'],
-    'p4':['d7','d7'],
-    'p5':['e7','e7'],
-    'p6':['f7','f7'],
-    'p7':['g7','g7'],
-    'p8':['h7','h7']
-}
+record=start_read(source)
+PGNrecord="""
+[Event "Hoogovens Group A"]
+[Site "Wijk aan Zee NED"]
+[Date "1999.01.20"]
+[EventDate "1999.01.16"]
+[Round "4"]
+[Result "1-0"]
+[White "Garry Kasparov"]
+[Black "Veselin Topalov"]
+[ECO "B07"]
+[WhiteElo "2812"]
+[BlackElo "2700"]
+[PlyCount "87"]
+
+"""
+result=decode_stringhistory(record)
+PGNrecord+=result
+print(PGNrecord)
+
+
+
+
+
+
 
 
 
